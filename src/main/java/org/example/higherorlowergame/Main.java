d3babe6e9e931e2828873a3840d5453063351529
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2024-12-01 20:44:17 +0000

    created a main to start the game

---
+++ src/main/java/org/example/higherorlowergame/Main.java
@@ -0,0 +1,12 @@
+package org.example.higherorlowergame;
+
+import org.example.higherorlowergame.GUI.GameApplication;
+
+/**
+ * @author      Abdelrahmane, Bekhli, abdelrahmane001@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        GameApplication.main(args); // starts the application
+    }
+}
